f40bf7e62c9867816fd73781524bcb2ef983f2e1
Damon Gudaitis <damon@caretjuice>
2021-06-17 15:04:07 -0700

    modified forms for Netlify

--- index.js
+++ index.js
@@ -2,56 +2,41 @@ const log = msg => {
     document.getElementById('log').innerHTML += msg + "\n";
 };
 
+function sendFormData() {
 
-window.addEventListener("DOMContentLoaded", function () {
-    // Access the form element...
-  const form = document.getElementById( "send_webhook" );
-
-  // ...and take over its submit event.
-  document.getElementById("submit_webhook").addEventListener( "click", function ( event ) {
-    console.log("Submitting form");
-    form.sendWebhookData();
-    event.preventDefault();
-  } );
-});
-
-function sendWebhookData() {
-  const xhr = new XMLHttpRequest();
-
-  // Bind the FormData object and the form element
-  const fd = new FormData( form );
-  log(fd);
-  console.log(fd);
-  url = fd.destination_url
-  log(url);
-  console.log(url);
-  json = fd.json_to_send
-  console.log(json);
-  log(json);
-
-  // Define what happens on successful data submission
-  xhr.addEventListener( "load", function(event) {
-    log( event.target.responseText );
-  } );
-
-  // Define what happens in case of error
-  xhr.addEventListener( "error", function( event ) {
-    this.LOADING( 'Oops! Something went wrong.' );
-  } );
-
-  // Set up our request
-  xhr.open( "POST", url );
-  xhr.setRequestHeader('Content-Type', 'application/json');
-
-  // The data sent is what the user provided in the form
-  xhr.send( json.stringify(json) );
-
-      //listen for response
-  xhr.onreadystatechange=(e) =>{
-    log( xhr.statusText );
-    log( xhr.responseURL );
-    log( xhr.responseType );
-    log( xhr.responseBody );
+  function sendData() {
+    var XHR = new XMLHttpRequest()
+    var FD = new FormData(form)
+    url = FD.destination_url
+    log(url);
+    console.log(url);
+    postJSON = FD.json_to_send
+    console.log(postJSON);
+    log(postJSON);
+    XHR.addEventListener('load', function(event) {
+      form.classList.add('inactive')
+      var success = document.querySelectorAll('.post-sent')[0]
+      success.classList.add('active')
+    })
+    XHR.addEventListener('error', function(event) {
+      form.classList.add('inactive')
+      var error = document.querySelectorAll('.post-error')[0]
+      error.classList.add('active')
+    })
+    XHR.setRequestHeader('Content-Type', 'application/json');
+    XHR.open('POST', url);
+    XHR.send(postJSON);
   }
-  
+
+  var form = document.querySelectorAll('.my-form')[0]
+  form.addEventListener('submit', function (e) {
+    e.preventDefault()
+    sendData()
+  })
+
 }
+
+log( XHR.statusText );
+log( XHR.responseURL );
+log( XHR.responseType );
+log( XHR.responseBody );
